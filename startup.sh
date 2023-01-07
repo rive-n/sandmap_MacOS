efbcafba0c376f3be361c71464881a0693ba8c4d
Saschenko Danila <d.saschenko@NB0304-4.local>
2023-01-07 20:21:12 +0300

    fix: typo

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Trying to install gnu-getopt and coreutils (we need GNU tools for sandmap)."
-brew install -q gnu-getopt
+brew install -q --overwrite gnu-getopt
 # brew link --force gnu-getopt - not recommended, but you can try.
-brew install -q coreutils
+brew install -q --overwrite coreutils
 
-# sudo ./setup.sh install
+sudo ./setup.sh install
